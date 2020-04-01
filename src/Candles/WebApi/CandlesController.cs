93231150ecab064580856b9ba3e9481b851ae7fd
Alexey Zelenin <alexey@zelenin.org>
2020-04-01 10:48:27 +0300

    Add authorize

--- src/Candles/WebApi/CandlesController.cs
+++ src/Candles/WebApi/CandlesController.cs
@@ -1,13 +1,15 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
 using Candles.Client.Models.Candles;
 using Candles.Domain.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Candles.WebApi
 {
+    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class CandlesController : ControllerBase
